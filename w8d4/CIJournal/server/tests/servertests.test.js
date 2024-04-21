3cd37a64ee4cb710db9862b9a2bbbf8c2bb075fc
JeremySpence272 <jeremy.spence272@gmail.com>
2024-04-21 15:15:47 -0400

    connect to mongo in test

--- w8d4/CIJournal/server/tests/servertests.test.js
+++ w8d4/CIJournal/server/tests/servertests.test.js
@@ -1,31 +1,33 @@
 const request = require("supertest");
 const app = require("../dist/app"); // Ensure this path correctly points to the compiled app
 
-let server;
+const { recordsDb } = require("../dist/dbConnections");
+const { default: mongoose } = require("mongoose");
 
-beforeAll((done) => {
-	server = app.listen(3004, () => {
-		global.agent = request.agent(server);
-		done();
-	});
+beforeAll(async () => {
+	await mongoose.connect(
+		"mongodb+srv://jeremyspence272:XXcctPB8g2tlOLH9@cijournal-cluster.nr4u260.mongodb.net/records"
+	);
 });
 
-afterAll((done) => {
-	server.close(done);
+afterAll(async () => {
+	await mongoose.disconnect();
 });
 
 describe("POST /new", () => {
 	test("should respond with 400 status if title or body is missing", async () => {
+		expect.assertions(1);
 		const response = await request(app).post("/new").send({
 			date: "04/21/2024",
 			title: "",
 			body: "Sample body",
 		});
 		expect(response.statusCode).toBe(400);
-	});
+	}, 20000);
 });
 describe("POST /new", () => {
 	test("should handle long text inputs", async () => {
+		expect.assertions(1);
 		const longText = "lorem ipsum".repeat(500);
 		const response = await request(app).post("/new").send({
 			date: "04/21/2024",
@@ -33,15 +35,16 @@ describe("POST /new", () => {
 			body: longText,
 		});
 		expect(response.statusCode).toBe(201);
-	});
+	}, 20000);
 });
 describe("POST /new", () => {
 	test("should handle special characters in input", async () => {
+		expect.assertions(1);
 		const response = await request(app).post("/new").send({
 			date: "04/21/2024",
 			title: "Title with special characters 😊 <script>",
 			body: "Body with emojis 😂 and symbols @#$%",
 		});
 		expect(response.statusCode).toBe(201);
-	});
+	}, 20000);
 });
